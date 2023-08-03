9ba0353ed6c329de9acbb5babcb2242bf34bdb71
Алина Шнайдер <shnaideralina@MacBook-Air-Alina.local>
2023-08-03 17:57:18 +0300

    route

---
+++ backend/config/serverConfig.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const sessionConfig = require('./sessionConfig');
+
+const serverConfig = (app) => {
+  app.use(morgan('dev'));
+  app.use(cookieParser());
+  app.use(session(sessionConfig));
+  app.use(express.urlencoded({extended: true})); // читать данные из тела запросов
+  app.use(express.json()); // читать JSON-данные из тела запросов
+};
+
+module.exports = serverConfig;
---
+++ backend/routes/index.route.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const apiAuthRouter = require('./api/apiAuth.route');
+
+router.use('/api/auth', apiAuthRouter);
+
+module.exports = router;
