2298ee160a3b3ed46c827fa789a7d7f045f7465d
ivo7643 <nahdora001@gmail.com>
2023-08-04 16:11:40 +0300

    css

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,35 +1,27 @@
-
 import React, { useEffect } from "react";
-import './App.css';
-import {Routes, Route} from 'react-router-dom';
-import Registration from '../features/Auth/Registration';
-import QuestionsList from '../features/Questions/QuestionsList';
-
-
-
-
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Registration from "../features/Auth/Registration";
+import QuestionsList from "../features/Questions/QuestionsList";
 
 import ModalAnswer from "../features/Questions/ModalAnswer";
 import Navbar from "../features/Navbar/Navbar";
 import MainPage from "../features/MainPage/MainPage";
 import { useDispatch } from "react-redux";
-import * as api from '../features/Auth/api';
+import * as api from "../features/Auth/api";
 import Authorization from "../features/Auth/Authorization";
-
-
 
 function App() {
   const dispatch = useDispatch();
   useEffect(() => {
-    api.verificationFetch()
-    .then((data) => dispatch({ type: 'auth/verification', payload: data }));
+    api
+      .verificationFetch()
+      .then((data) => dispatch({ type: "auth/verification", payload: data }));
   }, []);
   return (
     <div className="App">
       <Navbar />
       <Routes>
-
         <Route path="/" element={<Registration />} />
         <Route path="/theme" element={<QuestionsList />} />
 
@@ -40,7 +32,6 @@ function App() {
         {/* <Route path="/themes/:idTheme" element={<Registration />} />
         <Route path="/question" element={<Registration />} />
         <Route path="/question/:idQuestion" element={<Registration />} /> */}
-
       </Routes>
     </div>
   );
